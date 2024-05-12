39d83c9d7a2b1165b88bf189102bae444e26e43e
Mark Needham <m.h.needham@gmail.com>
2024-05-12 11:27:02 +0100

    one chat-response working

--- ollama-parallel/app_v1.py
+++ ollama-parallel/app_v1.py
@@ -29,15 +29,16 @@ col1, col2 = st.columns(2)
 col1.write(f"# :blue[{model_1}]")
 col2.write(f"# :red[{model_2}]")
 
+meta_1 = col1.empty()
+meta_2 = col2.empty()
+
 body_1 = col1.empty()
 body_2 = col2.empty()
 
 
-async def run_prompt(placeholder, prompt, model):
-    with placeholder.container():
-        user = st.chat_message(name="user")
-        user.write(prompt)
-
+async def run_prompt(placeholder, meta, prompt, model):
+    tc = TokenCount(model_name="gpt-3.5-turbo")
+    start = time.time()
     stream = await client.chat.completions.create(
         model=model,
         messages=[{"role": "system", "content": "You are a helpful assistant."},
@@ -49,18 +50,20 @@ async def run_prompt(placeholder, prompt, model):
         chunk_content = chunk.choices[0].delta.content
         if chunk_content is not None:
             streamed_text = streamed_text + chunk_content
-            with placeholder.container():
-                user = st.chat_message(name="user")
-                user.write(prompt)
-                blah = st.chat_message(name="assistant")
-                blah.write(streamed_text)
-                
+            placeholder.write(streamed_text)
+            end = time.time()
+            time_taken = end-start
+            tokens = tc.num_tokens_from_string(streamed_text)
+            meta.info(f"""**Duration: :green[{time_taken:.2f} secs]**
+            **Eval count: :green[{tokens} tokens]**
+            **Eval rate: :green[{tokens / time_taken:.2f} tokens/s]**
+            """)
 
 
 async def main():
     await asyncio.gather(
-        run_prompt(body_1, prompt=prompt, model=model_1),
-        run_prompt(body_2, prompt=prompt, model=model_2)
+        run_prompt(body_1, meta_1, prompt=prompt, model=model_1),
+        run_prompt(body_2, meta_2, prompt=prompt, model=model_2)
     )
 
 if generate:
