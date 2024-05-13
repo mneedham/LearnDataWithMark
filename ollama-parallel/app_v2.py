7c0675c881e464fdc47fa30f4778fcab8e319557
Mark Needham <m.h.needham@gmail.com>
2024-05-13 17:57:49 +0100

    spinner

--- ollama-parallel/app_v2.py
+++ ollama-parallel/app_v2.py
@@ -2,19 +2,44 @@ import ollama
 import streamlit as st
 import asyncio
 import time
+import base64
 from openai import AsyncOpenAI
 from streamlit_extras.stylable_container import stylable_container
-
 
 title = "Running LLMs in parallel with Ollama"
 st.set_page_config(page_title=title, layout="wide")
 st.title(title)
+
+st.markdown("""
+<style>
+hr {
+    margin: -0.5em 0 0 0;
+    background-color: red;
+}
+p.prompt {
+    margin: 0;
+    font-size: 14px;
+}
+
+div.stChatMessage :has(div[data-testid="chatAvatarIcon-assistant"]) {
+    flex-direction: row-reverse;
+    text-align: right;
+}
+
+img.spinner {
+    margin: -1em 0 0 0;
+}
+</style>
+""", unsafe_allow_html=True)
 
 if not "messages1" in st.session_state:
     st.session_state.messages1 = []
 
 if not "messages2" in st.session_state:
     st.session_state.messages2 = []
+
+if not "input_disabled" in st.session_state:
+    st.session_state.input_disabled = False
 
 
 client = AsyncOpenAI(base_url="http://localhost:11434/v1", api_key="ignore-me")
@@ -28,6 +53,10 @@ models = [
 def clear_everything():
     st.session_state.messages1 = []
     st.session_state.messages2 = []
+    st.session_state.input_disabled = False
+
+def disable():
+    st.session_state.input_disabled = True
 
 with st.sidebar:
     with stylable_container(
@@ -39,29 +68,15 @@ with st.sidebar:
         }""",
     ):
         st.button("New Chat :speech_balloon:", on_click=clear_everything)
-
-    st.markdown("""
-    <style>
-    hr {
-        margin: -0.5em 0 0 0;
-        background-color: red;
-    }
-    p.prompt {
-        margin: 0;
-        font-size: 14px;
-    }
-    </style>
-    """, unsafe_allow_html=True)
     st.write("***")
 
     model_1_index = models.index("phi3:latest")
-    model_1 = st.selectbox("Model 1", options=models, index=model_1_index)
+    model_1 = st.selectbox("Model 1", options=models, index=model_1_index, disabled=st.session_state.input_disabled)
     model_2_index = models.index("llama3:latest")
-    model_2 = st.selectbox("Model 2", options=models, index=model_2_index)
+    model_2 = st.selectbox("Model 2", options=models, index=model_2_index, disabled=st.session_state.input_disabled)
     
-
     st.markdown("<p class='prompt'>Prompt</p>", unsafe_allow_html=True)
-    prompt = st.chat_input("Message Ollama")
+    prompt = st.chat_input("Message Ollama", on_submit=disable)
 
 col1, col2 = st.columns(2)
 col1.write(f"# :blue[{model_1}]")
@@ -72,20 +87,22 @@ body_2 = col2.empty()
 
 
 async def run_prompt(placeholder, model, message_history):
-    # with placeholder.container():
-    #     user = st.chat_message(name="user")
-    #     user.write(prompt)
-
     with placeholder.container():
         for message in message_history:
             chat_entry = st.chat_message(name=message['role'])
             chat_entry.write(message['content'])
+        assistant = st.chat_message(name="assistant")
+        # assistant.image("images/loading-gif.gif", width=25)
 
+        with open("images/loading-gif.gif", "rb") as file:
+            contents = file.read()
+            data_url = base64.b64encode(contents).decode("utf-8")
+
+        assistant.markdown(f"<img src='data:image/gif;base64,{data_url}' class='spinner' width='25' />", unsafe_allow_html=True)
 
     messages = [
         {"role": "system", "content": "You are a helpful assistant."},
-        *message_history,
-        {"role": "user", "content": prompt},
+        *message_history
     ]
     stream = await client.chat.completions.create(
         model=model,
@@ -106,17 +123,17 @@ async def run_prompt(placeholder, model, message_history):
     message_history.append({"role": "assistant", "content": streamed_text})
                 
 
-
 async def main():
     await asyncio.gather(
         run_prompt(body_1, model=model_1, message_history=st.session_state.messages1),
         run_prompt(body_2, model=model_2, message_history=st.session_state.messages2)
     )
+    st.session_state.input_disabled = False
 
 if prompt:
     if prompt == "":
         st.warning("Please enter a prompt")
-    else:
+    else:        
         st.session_state.messages1.append({"role": "user", "content": prompt})
         st.session_state.messages2.append({"role": "user", "content": prompt})
         asyncio.run(main())
